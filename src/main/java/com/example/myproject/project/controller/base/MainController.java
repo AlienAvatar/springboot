ab4f8b95560bd726852fa2c32de2ba3c869ab6b4
Avatar <dxf767808421@163.com>
2019-04-29 23:03:13 +0800

    dva

--- src/main/java/com/example/myproject/project/controller/base/MainController.java
+++ src/main/java/com/example/myproject/project/controller/base/MainController.java
@@ -1,7 +1,15 @@
 package com.example.myproject.project.controller.base;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/main")
 public class MainController {
+
+    @RequestMapping(value = "/dva", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    public String test(){
+        return "dva";
+    }
 }
