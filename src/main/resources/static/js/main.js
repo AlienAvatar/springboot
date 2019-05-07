31adf0c2ee270209a04568bda4f12fc71e8dd483
Avatar <dxf767808421@163.com>
2019-05-07 22:27:59 +0800

    更改主页

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,22 +2,32 @@ $(document).ready(function () {
     console.log("main.js")
 })
 function changeVideo1() {
-    $(".video-fluid")[0].src="/video/dva2.mp4"
+    document.getElementById("main-source").src = "http://localhost:8088/video/dva1.mp4";
+    document.getElementById("main-video").load();
+    $(".spotlight-image")[0].style.cssText = 'background-image: url("http://overwatch.nos.netease.com/1/images/v2/home/overlay/dva.png");';
+
 }
 
 function changeVideo2() {
-    $(".video-fluid")[0].src="/video/banzang2.mp4"
+    document.getElementById("main-source").src = "http://localhost:8088/video/banzang2.mp4";
+    document.getElementById("main-video").load();
+    $(".spotlight-image")[0].style.cssText = 'background-image: url("http://overwatch.nos.netease.com/1/images/v2/home/overlay/hanzo.png");';
 }
 
 function changeVideo3() {
-    $(".video-fluid")[0].src="/video/mcrae1.mp4"
+    document.getElementById("main-source").src = "http://localhost:8088/video/mcrae1.mp4";
+    document.getElementById("main-video").load();
+    $(".spotlight-image")[0].style.cssText = 'background-image: url("http://overwatch.nos.netease.com/1/images/v2/home/overlay/mccree.png");';
 }
 
 function changeVideo4() {
-    $(".video-fluid")[0].src="/video/star.mp4"
+    document.getElementById("main-source").src = "http://localhost:8088/video/reaper.mp4";
+    document.getElementById("main-video").load();
+    $(".spotlight-image")[0].style.cssText = 'background-image: url("http://overwatch.nos.netease.com/1/images/v2/home/overlay/reaper.png");';
 }
 function logOut() {
     window.location.href="http://localhost:8088/test";
+
 }
 
 var availableTags = [
--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -1,39 +1,28 @@
-$(document).ready(function () {
-    pageTools(1, 5);//tools里的方法
-    getList(1);
-});
-
-function getList(pageNo) {
-    $.ajax({
-        url: "/main/getList",
-        type: "GET",
-        data: "pageNo=" + pageNo + "&heroType=dva",
-        success: function (data) {
-            console.log(data);
-            if(data.statusCode == "200"){
-                var str = ' <tr style="border-bottom: 1px solid #919191;line-height: 35px;">\n' +
-                    '            <th style="text-align: center;">序号</th>\n' +
-                    '            <th style="text-align: center;">玩家ID</th>\n' +
-                    '            <th style="text-align: center;">英雄熟练度</th>\n' +
-                    '            <th style="text-align: center;">排名</th>\n' +
-                    '            <th></th>\n' +
-                    '        </tr>';
-                debugger;
-                var jsonData = JSON.parse(data.data);
-                for (var i = 0; i < jsonData.list.length; i++) {
-                    var num = i + 1;
-                    str +='<tr>'
-                        + '<td  style="padding: 14px;border-bottom: 1px solid #eee;width: 600px;text-align: center;">'+num+'</td>'
-                        + '<td  style="padding: 14px;border-bottom: 1px solid #eee;width: 600px;text-align: center;">'+jsonData.list[i].playerid+'</td>'
-                        + '<td  style="padding: 14px;border-bottom: 1px solid #eee;width: 600px;text-align: center;">'+jsonData.list[i].playerdop+'</td>'
-                        + '<td  style="padding: 14px;border-bottom: 1px solid #eee;width: 600px;text-align: center;">'+jsonData.list[i].playerrank+'</td>'
-                        + '</tr>';
-                    $('table').html(str);
-                }
-                //pageTools(1,30);
-            }else{
-                alert("无相关信息");
-            }
+function mixin(sourceObj,targetObj) {
+    for(var key in sourceObj){
+        if(!(key in targetObj)){
+            targetObj[key] = sourceObj[key];
         }
-    })
+    }
+    return targetObj;
 }
+
+var Vehicle = {
+    engines : 1,
+    ignition:function(){
+        console.log("Turning on my engine.");
+    },
+    drive:function () {
+        this.ignition();
+        console.log("Sterring and moving foward!");
+    }
+};
+
+var Car = mixin(Vehicle,{
+    wheels:4,
+
+    drive:function () {
+        Vehicle.drive().call(this);
+        console.log("Rolling on all" + this.wheels + "wheels!");
+    }
+});
